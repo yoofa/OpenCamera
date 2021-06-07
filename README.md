9f7047a77442f13b58b8439b8581ca1c7fbad66b
youfa.song <vsyfar@gmail.com>
2021-06-07 23:36:48 +0800

    code init

---
+++ README.md
@@ -0,0 +1,1 @@
+## ONVIF Cameres
---
+++ app/main.cc
@@ -0,0 +1,10 @@
+/*
+ * main.cc
+ * Copyright (C) 2021 youfa.song <vsyfar@gmail.com>
+ *
+ * Distributed under terms of the GPLv2 license.
+ */
+
+int main() {
+  return 0;
+}
